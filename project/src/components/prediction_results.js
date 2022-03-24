2d15b1b066128ae2612666b07e9c1ec08de3f8d4
Cazzzz <yeracscott9@gmail.com>
2022-03-24 16:05:14 +0000

    elements on page

--- project/src/components/prediction_results.js
+++ project/src/components/prediction_results.js
@@ -9,62 +9,17 @@ import {
   TableHead,
   TableRow,
 } from "@material-ui/core";
-import { ConstructionOutlined } from "@mui/icons-material";
+import { Code, ConstructionOutlined } from "@mui/icons-material";
 import React from "react";
 import "../positions.css";
+import parse from "html-react-parser";
 
 import { useState, useEffect } from "react";
 
 const PredictionResults = (props, importFrom) => {
-  // console.log(props.importFrom.predictions + 'index');
-
-  // const [predictions, setPredictions] = useState([]);
-  // setPredictions(props.importFrom.predictions);
-  // console.log(props + 'hello');
-
-  // const filteredPredictions = props.importFrom.predictions.filter(
-  //   (prediction) => prediction.probability >= 0.9
-  // );
-
-  function positioning(predictions) {}
   console.log(props.importFrom.predictions);
-  // props.importFrom.predictions.forEach(prediction => {
-
-  //     let left = prediction.boundingBox.left;
-
-  //      if (left < 0.2) {
-  //       console.log("Set in grid left- 1/1");
-  //       // setLeftPosition("component4");
-  //       prediction.boundingBox["position"] = 'div1';
-  //       console.log(prediction.boundingBox);
-  //     } else if (left > 0.2 && left < 0.3) {
-  //       console.log("grid left 2/2");
-  //       prediction.boundingBox["position"] = 'div1';
-  //     } else if (left > 0.3 && left < 0.4) {
-  //       console.log("grid left 3/3");
-  //       prediction.boundingBox["position"] = 'div1';
-  //     } else if (left > 0.4 && left < 0.5) {
-  //       console.log("grid left 4/4");
-  //       prediction.boundingBox["position"] = 'div1';
-  //     } else if (left > 0.5 && left < 0.6) {
-  //       console.log("grid left 5/5");
-  //       prediction.boundingBox["position"] = 'div1';
-  //     } else if (left > 0.6 && left < 0.7) {
-  //       console.log("grid left 6/6");
-  //       prediction.boundingBox["position"] = 'div1';
-  //     } else if (left > 0.7 && left < 0.8) {
-  //       console.log("grid left 7/7");
-  //       prediction.boundingBox["position"] = 'div1';
-  //     } else if (left > 0.8 && left < 0.9) {
-  //       console.log("grid left 8/8");
-  //       prediction.boundingBox["position"] = 'div1';
-  //     } else if (left > 0.9) {
-  //       console.log("grid left 9/9");
-  //       prediction.boundingBox["position"] = 'component9';
-  //     }
-
-  //   });
-  // .filter(filteredPredictions[0].probability <= 0.1)
+  console.log(props.code);
+  console.log(props);
 
   function positioning(props) {
     if (isLoggedIn) {
@@ -73,74 +28,49 @@ const PredictionResults = (props, importFrom) => {
     return <GuestGreeting />;
   }
 
+  function checkTagName(prediction, props) {
+    let value;
+    console.log(prediction);
+    props.code.forEach((element, index) => {
+      if (element.tagName === prediction.tagName) {
+        value = element;
+      }
+    });
+    return value;
+  }
+
   return (
     <>
-      {/* <TableContainer >
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Name</TableCell>
-            <TableCell align="right"> Top</TableCell>
-            <TableCell align="right">Width</TableCell>
-            <TableCell align="right">Height</TableCell>
-
-            <TableCell align="right">Probability</TableCell>
-
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          { props.importFrom.predictions.map((prediction, i) => (
-            <TableRow
-              key={i}
-              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {prediction.tagName}
-              </TableCell>
-              <TableCell align="right">{prediction.probability}</TableCell>
-              <TableCell align="right">{prediction.boundingBox.left}</TableCell>
-              <TableCell align="right">{prediction.boundingBox.top}</TableCell>
-              <TableCell align="right">{prediction.probability}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer> */}
-
       {props.importFrom.predictions.map((prediction, i) => {
-        //   return (prediction.boundingBox.range = "nextRow") ?
-        //   (<>
-        //   <div key={`spacing-${i}`} className={`${prediction.boundingBox.pushLeft}  flex flexWidth`}>&nbsp;</div>
-
-        //   <div key={`object-${i}`} className={`${prediction.boundingBox.width} flex flexWidth`}>
-        //   Object
-        // </div>
-        // <br />
-        // </>
-        // ) :
-
-        // (<div key={`solo-object-${i}`} className={`${prediction.boundingBox.width} flex flexWidth`}>
-        //    Object
-        //  </div>)
+        // document.getElementById("element").innerHTML = codeResponse();
 
         {
-          if (prediction.boundingBox.range === "nextRow") {
+          if (props.isLoading) {
+            return <div className="">Loading...</div>;
+          } else if (prediction.boundingBox.range === "nextRow") {
             return (
               <>
                 <div className="break flex"></div>
                 <div
                   key={`spacing-${i}`}
-                  className={`${prediction.boundingBox.pushLeft}  flex flexWidth`}
+                  className={`element ${prediction.boundingBox.pushLeft}  flex flexWidth`}
                 >
-               {prediction.code}
-
-                  {i}
+                  {/* {props.code.tagName === prediction.tagName} */}
+                  {/* {i} */}
                 </div>
                 <div
                   key={`object-${i}`}
                   className={`${prediction.boundingBox.width} flex flexWidth`}
                 >
-                  Object {i}
+                  {/* {props.code.forEach((code) => {
+                    if (code.tagName === prediction.tagName) {
+                      let codeReturned = code.tagName;
+                      console.log(codeReturned);
+                      return code.tagName ? "Loading" : code.tagName;
+                    }
+                  })} */}
+
+                  {parse(checkTagName(prediction, props).code)}
                 </div>
               </>
             );
@@ -148,22 +78,27 @@ const PredictionResults = (props, importFrom) => {
             return (
               <>
                 <div
-                  key={`spacing-${i}`}
+                  key={`first-${i}`}
                   className={`${prediction.boundingBox.pushLeft}  flex flexWidth`}
                 >
-               {i}
+                  {/* {i} */}
                 </div>
-                <div key={`spacing-${i}`}>First {i}</div>
+                <div
+                  key={`same-${i}`}
+                  className={`flex  ${prediction.boundingBox.width}`}
+                >
+                  {parse(checkTagName(prediction, props).code)}
+                </div>
               </>
             );
           } else {
             return (
               <>
                 <div
-                  key={`spacing-${i}`}
+                  key={`same-${i}`}
                   className={`flex  ${prediction.boundingBox.width}`}
                 >
-                  Same {i}
+                  {parse(checkTagName(prediction, props).code)}{" "}
                 </div>
               </>
             );
--- project/src/pages/Testing.js
+++ project/src/pages/Testing.js
@@ -6,72 +6,11 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import Hello from '../components/prediction_results';
 
-function Positioning(prediction) {
 
-    // const [leftPosition, setLeftPosition] = useState('');
-    // let leftPositionString = leftPosition.toString();
-  
-  
-    if (!prediction) return null;
-  
-    if (prediction.predictions) {
-      let left = prediction.predictions[0].boundingBox.left;
-      let top = prediction.predictions[0].boundingBox.top;
-  
-      let width = prediction.predictions[0].boundingBox.width;
-      let height = prediction.predictions[0].boundingBox.height;
-  
-      // console.log(left, top, width, height);
-  
-      // if (left < 2) {
-      //   console.log("Set in grid - 1/1");
-      //   setLeftPosition("component4");
-      // } else if (left > 2 && left < 3) {
-      //   console.log("grid 2/2");
-      // } else if (left > 3 && left < 4) {
-      //   console.log("grid 3/3");
-      // } else if (left > 4 && left < 5) {
-      //   console.log("grid 4/4");
-      // } else if (left > 5 && left < 6) {
-      //   console.log("grid 5/5");
-      // } else if (left > 6 && left < 7) {
-      //   console.log("grid 6/6");
-      // } else if (left > 7 && left < 8) {
-      //   console.log("grid 7/7");
-      // } else if (left > 8 && left < 9) {
-      //   console.log("grid 8/8");
-      // } else if (left > 9) {
-      //   console.log("grid 9/9");
-      // }
-  
-      // if ( top < 2 ){
-      //     console.log('Set in grid - 1/1');
-      // } else if(top > 2 && top < 3){
-      //     console.log('grid 2/2');
-      // } else if(top > 3 && top < 4){
-      //     console.log('grid 3/3');
-      // }else if(top > 4 && top < 5){
-      //     console.log('grid 4/4');
-      // }else if(top > 5 && top < 6){
-      //     console.log('grid 5/5');
-      // }else if(top > 6 && top < 7){
-      //     console.log('grid 6/6');
-      // }else if(top > 7 && top < 8){
-      //     console.log('grid 7/7');
-      // }else if(top > 8 && top < 9){
-      //     console.log('grid 8/8');
-      // }else if(top > 9){
-      //     console.log('grid 9/9');
-      // }
-    }
-  
-}
 const Testing = (props, leftPosition) => {
   const [prediction, setPrediction] = useState({});
+  const [code, setCode] = useState({});
   const [isLoading, setLoading] = useState(true);
-
-
-
 
   let token = localStorage.getItem("token");
 
@@ -87,13 +26,39 @@ const Testing = (props, leftPosition) => {
         setPrediction(response.data);
         setLoading(false);
 
+        // console.log(setPredictions);
+      })
+      .catch((err) => {
+        console.log(`Error: ${err}`);
+      });
+
+
+  }, [token]);
+
+
+  useEffect(() => {
+        axios
+      .get(`http://localhost:3030/code/react`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((response) => {
+        // console.log(response.data);
+        setCode(response.data);
+        // setLoading(false);
+
         
         // console.log(setPredictions);
       })
       .catch((err) => {
         console.log(`Error: ${err}`);
       });
+
   }, [token]);
+  
+
+
 
   
 // console.log(leftPosition);
@@ -105,7 +70,7 @@ const Testing = (props, leftPosition) => {
   return (
     <>
     <div className="container">
-        <Hello importFrom={prediction}/>
+        <Hello importFrom={prediction} code={code}/>
         </div>
     </>
   );
