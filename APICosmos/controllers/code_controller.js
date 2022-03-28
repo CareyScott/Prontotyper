9616241034ebdc4586bc5ed4e14a3b08d9d24fe4
Cazzzz <yeracscott9@gmail.com>
2022-03-28 20:09:26 +0100

    attempting fix

--- APICosmos/controllers/code_controller.js
+++ APICosmos/controllers/code_controller.js
@@ -1,5 +1,16 @@
-const FileSaver = require('file-saver');
-const Blob = require('node-blob');
+const {FileSaver} = require("file-saver");
+const Blob = require("buffer");
+const { DOMParser } = require('xmldom')
+fs = require('fs'),path = require("path"),
+util = require("util");
+
+
+
+// const FileAPI = require('file-api')  , File = FileAPI.File
+// , FileList = FileAPI.FileList
+// , FileReader = FileAPI.FileReader
+// ;
+const { BlobServiceClient } = require("@azure/storage-blob");
 
 
 const res = require("express/lib/response");
@@ -7,13 +18,12 @@ const Code = require("../models/code_schema");
 const User = require("../models/user_schema");
 const Prediction = require("../models/prediction_schema");
 
-const { BlobServiceClient } = require("@azure/storage-blob");
-
 const blobSasUrl =
   "https://sketch2codestoresc.blob.core.windows.net/?sv=2020-08-04&ss=bfqt&srt=sco&sp=rwdlacupitfx&se=2022-03-28T19:07:26Z&st=2022-03-28T11:07:26Z&spr=https&sig=JqMKzkesXCjckyf63hP659XYg6lv1GeZYFfHkoY4l%2Fg%3D";
 const blobServiceClient = new BlobServiceClient(blobSasUrl);
-const containerName = "567567";
+const containerName = "container" + new Date().getTime();
 const containerClient = blobServiceClient.getContainerClient(containerName);
+
 
 // const getAllCode = (req, res) => {
 //   Code.find()
@@ -83,61 +93,148 @@ const generateFile = (prediction, code) => {
           >
             ${checkTagName(prediction).code}
           </div>
-          `
+          `;
     }
-
- 
   });
 
- 
-
-  
   // save file in container as componentName.js
-  const addContainer = async () => {
- 
-
-    // console.log(containerClient)
-    const createContainerResponse = await containerClient.create();
-    console.log(
-      "Container was created successfully. requestId: ",
-      createContainerResponse.requestId
-    );
-  };
-  addContainer();
-
-  
-
-  const saveCodeToContainer = async (html) => {
-
-    if (html){
-      let code = new Blob([html], {type: "text/html"});
-      html === code
-      return html 
+  const createContainer = async () => {
+    try {
+        console.log(`Creating container "${containerName}"...`);
+        await containerClient.createIfNotExists();
+        // reportStatus(`Done.`);
+        console.log('Created container ')
+        uploadFiles(html)
+    } catch (error) {
+        console.log(error.message);
     }
-   FileSaver.saveAs(html, "code.html");
+
+
+};
+
+createContainer()
+
+  // addContainer();
+
+
+  // const saveCodeToContainer = async (html) => {
+  //   // if (html) {
+  //   //   let code = new Blob([html], { type: "text/html" });
+  //   //   html === code;
+  //   //   return html;
+  //   // }
+  //   // FileSaver.saveAs(html, "code" + uuidv1() + ".html");
+
+  //   // console.log(html)
+  //   try {
+  //     const containerName = "8888";
+  //     const containerClient = blobServiceClient.getContainerClient(containerName);
+
+
+  //     const blobName = "code" + uuidv1() + ".txt"
+  //     const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+
+  //     console.log("\nUploading to Azure storage as blob:\n\t", blobName);
+
+  //     // Upload data to the blob
+  //     // const data = html;
+  //     const uploadBlobResponse = await blockBlobClient.upload(
+  //       html.toString(),
+  //       html.length
+  //     );
+  //     console.log(
+  //       "Blob was uploaded successfully. requestId: ",
+  //       uploadBlobResponse.requestId
+  //     );
+  //     // reportStatus("Done.");
+  //     // listFiles();
+  //   } catch (error) {
+  //     // reportStatus(error.message);
+  //     console.log("something went wrong uploading the file");
+  //   }
+  // };
+
+  // saveCodeToContainer(html)
+
+
+  // const uploadFiles = async () => {
+  //     // var parser = new DOMParser();
+  //     // var doc = parser.parseFromString(html, 'text/html');
+
+  //     // var file = new File([`${html}`], "filename");
+  //     // var f = new File([""], "filename.txt", {type: "text/plain"})
+
+  //     // return doc.body;
     
-   try {
-    // reportStatus("Uploading files...");
-    const promises = [];
-    html.forEach(file => {
+  //   //  FileSaver.saveAs(html, "code" + uuidv1() + ".html");
+  //   const data = "Hello, World!";
+
+  //   try {
+  //     // reportStatus("Uploading files...");
+  //     // const promises = [];
+  //     // doc.forEach(file => {
+  //       // let file = doc
+  //     console.log(data);
+
+  //      //  get component name here
+  //      // let filename = componentName + projectID + file.ext;
+  //       const blockBlobClient = containerClient.getBlockBlobClient(file.name);
+  //       push(blockBlobClient.uploadData(data));
+  //     // })
+
+  //     // await Promise.all(promises);
+  //     // reportStatus("Done.");
+  //     // listFiles();
+  //   } catch (error) {
+  //     // reportStatus(error.message);
+  //     console.log("comething went wrong uploading the file");
+  //   }
+    
+  // };
+  // uploadFiles()
+
+
+  const uploadFiles = async (html) => {
+    let file = fs.writeFile('helloworld.txt', 'Hello World!', function (err) {
+      if (err) return console.log(err);
+      
+    }    );
+
+
+    var content;
+console.log(content);
+fs.readFile( 'utf8', function (err, data) {
+    if (err) {
+        console.log(err);
+        process.exit(1);
+    }
+    content = util.format(data, "test", "test", "test");
+    console.log(content);
+});
+   
+
+    if (!file){
       console.log(file);
+    }
 
-     //  get component name here
-     // let filename = componentName + projectID + file.ext;
-      const blockBlobClient = containerClient.getBlockBlobClient(file.name);
-      promises.push(blockBlobClient.uploadData(file));
-    })
+    try {
+      console.log("Uploading files...");
+        const promises = [];
+        if (file) {
+            const blockBlobClient = containerClient.getBlockBlobClient(file.name);
+            promises.push(blockBlobClient.uploadBrowserData(file));
+        }
+        await Promise.all(promises);
+        console.log("Done.");
+        // listFiles();
+    }
+    catch (error) {
+            console.log(error.message);
+    }
+}
 
-    await Promise.all(promises);
-    // reportStatus("Done.");
-    // listFiles();
-  } catch (error) {
-    // reportStatus(error.message);
-    console.log("comething went wrong uploading the file");
-  }
-  };
 
-  saveCodeToContainer(html);
+
 };
 
 const downloadCode = (req, res) => {
